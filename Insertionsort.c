39f6d25f9628b031e649337b67c81970704ccae3
Barath-N-P <barathnatarajan234@gmail.com>
2026-04-04 21:05:25 +0530

    Add files via upload

---
+++ Insertionsort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n,next=0;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(int i=1;i<n;i++)
+    {
+        int y=i;
+        while(y>0 && a[y] <a[y-i])
+        {
+            int temp=a[y];
+            a[y]=a[y-i];
+            a[y-i]=temp;
+            y--;
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+}
